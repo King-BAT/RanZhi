18c1fc7de2f43d81931182222542aabb76fe92ef
sunliying666 <736624557@qq.com>
2018-11-26 21:28:32 +0800

    JavaUI框架

---
+++ UI_Test_Framework/src/utils/ReadProperties.java
@@ -0,0 +1,26 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ReadProperties {
+
+	static String filePath = "conf/config.properties";
+	static String eMail="conf/email.properties";
+
+	public static String getPropertyValue(String key) throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(filePath);
+		prop.load(fis);
+		fis.close();
+		return prop.getProperty(key);
+	}
+	public static String getEmailPropertyValue(String key) throws IOException {
+		Properties prop = new Properties();
+		FileInputStream fis = new FileInputStream(eMail);
+		prop.load(fis);
+		fis.close();
+		return prop.getProperty(key);
+	}
+}
